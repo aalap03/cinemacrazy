a4baf98665fb5b19cd8769ddfc983e3b709873d4
aalap <apatel@e-cobalt.com>
2019-03-26 17:32:38 -0400

    drawer added with sub items.

--- app/src/main/java/com/example/cinemacrazy/datamodel/utils/constants.kt
+++ app/src/main/java/com/example/cinemacrazy/datamodel/utils/constants.kt
@@ -9,3 +9,20 @@ val MEDIA_TYPE_IMAGES = "images"
 val MEDIA_TYPE_VIDEOS = "videos"
 val KEY_MEDIA_TYPE = "media_type"
 val KEY_MEDIA_IDS = "media_ids"
+
+const val TRENDING_MOVIE = 1L
+const val NOW_PLAYING_MOVIE = 2L
+const val POPULAR_MOVIE = 3L
+const val TOP_RATED_MOVIE = 4L
+const val UPCOMING_MOVIE = 5L
+
+
+const val TRENDING_TV = 6L
+const val LATEST_TV = 7L
+const val POPULAR_TV = 8L
+const val TOP_RATED_TV = 9L
+const val AIRING_TODAY_TV = 10L
+
+
+
+
--- app/src/main/java/com/example/cinemacrazy/medialist/CinemaListActivity.kt
+++ app/src/main/java/com/example/cinemacrazy/medialist/CinemaListActivity.kt
@@ -2,35 +2,23 @@ package com.example.cinemacrazy.medialist
 
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.paging.PagedList
 import androidx.recyclerview.widget.GridLayoutManager
 import co.zsmb.materialdrawerkt.builders.DrawerBuilderKt
-import co.zsmb.materialdrawerkt.builders.drawer
-import co.zsmb.materialdrawerkt.draweritems.badgeable.primaryItem
-import co.zsmb.materialdrawerkt.draweritems.badgeable.secondaryItem
-import co.zsmb.materialdrawerkt.draweritems.divider
 import com.example.cinemacrazy.BaseActivity
 import com.example.cinemacrazy.R
-import com.example.cinemacrazy.application.App
-import com.example.cinemacrazy.application.AppDb
 import com.example.cinemacrazy.datamodel.serverResponses.cinemaResponses.BaseMedia
-import com.example.cinemacrazy.datamodel.utils.CINEMA_TYPE_MOVIE
-import com.example.cinemacrazy.datamodel.utils.CINEMA_TYPE_TV
+import com.example.cinemacrazy.datamodel.utils.*
 import com.mikepenz.materialdrawer.Drawer
-import com.mikepenz.materialdrawer.DrawerBuilder
-import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.ExpandableDrawerItem
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 import kotlinx.android.synthetic.main.media_list.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
-import kotlin.concurrent.thread
 
 class CinemaListActivity : BaseActivity(), AnkoLogger {
 
@@ -48,32 +36,16 @@ class CinemaListActivity : BaseActivity(), AnkoLogger {
         drawer = DrawerBuilderKt(this)
             .builder
             .withToolbar(cinema_list_toolbar)
+            .withDisplayBelowStatusBar(true)
+            .withActionBarDrawerToggleAnimated(true)
             .build()
-        setupDrawer()
-
-        supportActionBar?.title = ""
+        addDrawerItems()
 
         setRecyclerView()
-        //setSpinner()
 
         viewmodel = ViewModelProviders.of(this).get(CinemaViewModel::class.java)
         displayLiveMedia(CINEMA_TYPE, null, "")
 
-    }
-
-    private fun setupDrawer() {
-
-        drawer.addItems(
-            PrimaryDrawerItem().withName("Aalap"),
-            SecondaryDrawerItem().withName("Ankita").withOnDrawerItemClickListener(object: Drawer.OnDrawerItemClickListener {
-                override fun onItemClick(view: View?, position: Int, drawerItem: IDrawerItem<*, *>?): Boolean {
-
-                    return true
-                }
-            }),
-            DividerDrawerItem(),
-            SecondaryDrawerItem().withName("Ankita")
-        )
     }
 
     private fun setRecyclerView() {
@@ -81,13 +53,6 @@ class CinemaListActivity : BaseActivity(), AnkoLogger {
         adapter = MoviesAdapter()
         recycler_view.adapter = adapter
     }
-
-//    private fun setSpinner() {
-//        media_selector.setItems("Trending Movies", "Trending TV Shows")
-//        media_selector.setOnItemSelectedListener { _, position, _, _ ->
-//            displayLiveMedia(if (position == 0) CINEMA_TYPE_MOVIE else CINEMA_TYPE_TV, null, CINEMA_LIST_TYPE)
-//        }
-//    }
 
     override fun getLayoutRes(): Int {
         return R.layout.media_list
@@ -115,21 +80,63 @@ class CinemaListActivity : BaseActivity(), AnkoLogger {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-            R.id.main_menu_now_playing -> displayLiveMedia(CINEMA_TYPE, null, NOW_PLAYING)
-            R.id.main_menu_upcoming -> displayLiveMedia(CINEMA_TYPE, null, UPCOMING)
-            R.id.main_menu_popular -> displayLiveMedia(CINEMA_TYPE, null, POPULAR)
-            R.id.main_menu_top_rated -> displayLiveMedia(CINEMA_TYPE, null, TOP_RATED)
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
     private fun displayLiveMedia(mediaType: String, query: String?, cinemaListType: String) {
         viewmodel.setCurrentMediaType(mediaType)
         viewmodel.getCurrentMediaType().observe(this, Observer<String> {
             viewmodel.getMediaLive(api, it, query = query, cinemaListType = cinemaListType).observe(this,
                 Observer<PagedList<BaseMedia>?> { t -> adapter.submitList(t) })
         })
+    }
+
+    fun showBackArrow() {
+        drawer.actionBarDrawerToggle.isDrawerIndicatorEnabled = false
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun showHamburgerIcon() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        drawer.actionBarDrawerToggle.isDrawerIndicatorEnabled = true
+    }
+
+    private fun addDrawerItems() {
+        drawer.addItem(PrimaryDrawerItem().apply {
+            withName("Cinem@Cr@zy")
+            withSelectable(false)
+            withSubItems()
+        })
+
+        drawer.addItem(ExpandableDrawerItem().apply {
+            withName("Movies")
+            withSubItems(
+                getSecondaryItemWithName(getString(R.string.movie_trending), TRENDING_MOVIE),
+                getSecondaryItemWithName(getString(R.string.movie_now_playing), NOW_PLAYING_MOVIE),
+                getSecondaryItemWithName(getString(R.string.movie_popular), POPULAR_MOVIE),
+                getSecondaryItemWithName(getString(R.string.movie_top_rated), TOP_RATED_MOVIE),
+                getSecondaryItemWithName(getString(R.string.movie_upcoming), UPCOMING_MOVIE)
+            )
+        })
+
+        drawer.addItem(ExpandableDrawerItem().apply {
+            withName("TV")
+            withSubItems(
+                getSecondaryItemWithName(getString(R.string.tv_trending), TRENDING_TV),
+                getSecondaryItemWithName(getString(R.string.tv_latest), LATEST_TV),
+                getSecondaryItemWithName(getString(R.string.tv_popular), POPULAR_TV),
+                getSecondaryItemWithName(getString(R.string.tv_top_rated), TOP_RATED_TV),
+                getSecondaryItemWithName(getString(R.string.tv_airing_today), AIRING_TODAY_TV)
+            )
+        })
+    }
+
+    private fun getSecondaryItemWithName(name: String, identifier: Long): SecondaryDrawerItem {
+        return SecondaryDrawerItem().apply {
+            withName(name)
+            withIdentifier(identifier)
+            withOnDrawerItemClickListener { view, position, drawerItem ->
+                info { "Item: identifier ${drawerItem.identifier}" }
+                info { "Item: position $position" }
+             false
+            }
+        }
     }
 }
